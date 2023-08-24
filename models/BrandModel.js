11385b5b32b37bb112eeecc68919d8ed287aa608
LeKyAnh1702 <lekyanhsd@gmail.com>
2023-08-24 08:46:33 +0700

    ud

--- models/BrandModel.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require ('mongoose');
-var BrandSchema = mongoose.Schema({
-   name: String,
-   
-});
-const BrandModel = mongoose.model('brand', BrandSchema, 'brand');
-module.exports = BrandModel;
